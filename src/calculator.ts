4055e2d5a44a9e75e15cc7f4b6b6f596c9ad3ae7
DanialNajafi <145651640+DanialNajafi@users.noreply.github.com>
2025-03-27 22:58:01 +0100

    Update calculator.ts

--- src/calculator.ts
+++ src/calculator.ts
@@ -9,5 +9,8 @@ export function multiply(a: number, b: number): number {
   return a * b;
 }
 export function divide(a: number, b: number): number {
+  if (b === 0) {
+    throw new Error('Division durch 0 ist nicht erlaubt');
+  }
   return a / b;
 }
